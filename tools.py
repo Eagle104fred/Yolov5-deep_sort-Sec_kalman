08b68da26beabd0c4f9591d2ee099b8082a73701
Eagle104fred <645040567@qq.com>
2021-04-26 13:02:31 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,44 +3,44 @@ class Tools:
         self.palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
 
         # 预测框解码，返回框中点和宽高
-        def bbox_rel(*xyxy):
-            """" Calculates the relative bounding box from absolute pixel values. """
-            bbox_left = min([xyxy[0].item(), xyxy[2].item()])
-            bbox_top = min([xyxy[1].item(), xyxy[3].item()])
-            bbox_w = abs(xyxy[0].item() - xyxy[2].item())
-            bbox_h = abs(xyxy[1].item() - xyxy[3].item())
-            x_c = (bbox_left + bbox_w / 2)
-            y_c = (bbox_top + bbox_h / 2)
-            w = bbox_w
-            h = bbox_h
-            return x_c, y_c, w, h
+    def bbox_rel(self,*xyxy):
+        """" Calculates the relative bounding box from absolute pixel values. """
+        bbox_left = min([xyxy[0].item(), xyxy[2].item()])
+        bbox_top = min([xyxy[1].item(), xyxy[3].item()])
+        bbox_w = abs(xyxy[0].item() - xyxy[2].item())
+        bbox_h = abs(xyxy[1].item() - xyxy[3].item())
+        x_c = (bbox_left + bbox_w / 2)
+        y_c = (bbox_top + bbox_h / 2)
+        w = bbox_w
+        h = bbox_h
+        return x_c, y_c, w, h
 
-        def compute_color_for_labels(label):
-            """
-            Simple function that adds fixed color depending on the class
-            """
-            color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
-            return tuple(color)
+    def compute_color_for_labels(self,label):
+        """
+        Simple function that adds fixed color depending on the class
+        """
+        color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+        return tuple(color)
 
-        def draw_boxes(img, bbox, clses, confs, names, identities=None, offset=(0, 0)):
-            for i, box in enumerate(bbox):
-                x1, y1, x2, y2 = [int(i) for i in box]
-                x1 += offset[0]
-                x2 += offset[0]
-                y1 += offset[1]
-                y2 += offset[1]
-                # box text and bar
-                id = int(identities[i]) if identities is not None else 0
-                color = compute_color_for_labels(id)
-                label = '{}{:d}'.format("", id)
-                cls = clses[i]
-                conf = confs[i]
-                class_str = f'{names[int(cls)]}'
-                text = label + ':' + class_str + ':' + str(conf)
-                t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
-                cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
-                cv2.rectangle(
-                    img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
-                cv2.putText(img, text, (x1, y1 +
-                                        t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
-            return img
+    def draw_boxes(self,img, bbox, clses, confs, names, identities=None, offset=(0, 0)):
+        for i, box in enumerate(bbox):
+            x1, y1, x2, y2 = [int(i) for i in box]
+            x1 += offset[0]
+            x2 += offset[0]
+            y1 += offset[1]
+            y2 += offset[1]
+            # box text and bar
+            id = int(identities[i]) if identities is not None else 0
+            color = compute_color_for_labels(id)
+            label = '{}{:d}'.format("", id)
+            cls = clses[i]
+            conf = confs[i]
+            class_str = f'{names[int(cls)]}'
+            text = label + ':' + class_str + ':' + str(conf)
+            t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+            cv2.rectangle(
+                img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
+            cv2.putText(img, text, (x1, y1 +
+                                    t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+        return img
