d337a95153b22645e4eb1a1b38bfaa3ef48b767c
Eagle104fred <645040567@qq.com>
2021-06-04 21:17:05 +0800

    更新视频源

--- run_YoSort.py
+++ run_YoSort.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                         default='yolov5/weights/2021.5.13_best.pt', help='model.pt path')#yolov5检测模型
     # file/folder, 0 for webcam
     parser.add_argument('--source', type=str,
-                        default='', help='source')#修改默认的检测源
+                        default='../TrafficFlow/testVideo.mp4', help='source')#修改默认的检测源
     parser.add_argument('--output', type=str, default='inference/output',
                         help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=640,
